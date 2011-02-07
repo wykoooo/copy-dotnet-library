192bd9c2600a39c81b177799e7a96fa7d4a7fe8b
silmoon.inc@gmail.com <silmoon.inc@gmail.com@53721017-0d7d-3a63-7736-a47e0384711a>
2011-02-07 13:24:29 +0000

    new silmoon.data class, support native mysql class.

--- trunk/Silmoon.Data/ISMSQL.cs
+++ trunk/Silmoon.Data/ISMSQL.cs
@@ -8,15 +8,17 @@ namespace Silmoon.Data
 {
     public interface ISMSQL
     {
+        ConnectionState State { get; }
         void Open();
         void Close();
-        int ExecNonQuery(string sqlCommand);
+        int ExecNonQuery(string sqlcommand);
         int GetRecordCount(string sqlcommand);
         object GetDataReader(string sqlcommand);
         object GetCommand(string sqlcommand);
         object GetDataAdapter(string sqlcommand);
         DataTable GetDataTable(string sqlcommand);
         object GetFieldObjectForSingleQuery(string tablename, string resulefield, string fieldname, string fieldvalue);
+        object GetFieldObjectForSingleQuery(string sqlcommand, bool isUseReader);
         object GetFieldObjectForSingleQuery(string sqlcommand);
         int UpdateFieldForSingleQuery(string tablename, string updatefield, string updatevalue, string fieldname, string fieldvalue);
         bool ExistRecord(string sqlcommand);
